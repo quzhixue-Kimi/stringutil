b7d97e4beeb96635c189649c84730825ffbea652
Kimi-Qu <eyres.jones@gmail.com>
2021-01-20 18:15:14 +0800

    code changed

--- stringutil.go
+++ stringutil.go
@@ -8,6 +8,7 @@ func init() {
 
 // Reverse returns its argument string reversed rune-wise left to right.
 func Reverse(s string) string {
+	fmt.Println("what is up?")
 	r := []rune(s)
 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
 		r[i], r[j] = r[j], r[i]
